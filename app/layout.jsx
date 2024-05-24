518c79c99fbbc649eb053160aec299ea1b56edd6
= <mturzynskanj@gmail.com>
2024-05-24 17:51:15 -0400

    Initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "@/assets/styles/globals.css";
+
+export const metadata = {
+  title: "PropertyPulse | Find perfect rental",
+  description: "Find your dream rental prop",
+  keywords: "rental, find rentals",
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div>test</div>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
